daaa29e6624fb94b71e3702e8d790e509a3fa5ed
pk049 <kumbharp049@gmail.com>
2025-04-18 23:42:52 +0530

    subs

--- server2.py
+++ server2.py
@@ -7,16 +7,22 @@ CORS(app)  # Allow requests from Flutter
 
 # Connect to MongoDB
 client = MongoClient("mongodb+srv://21uai019:O2OLlukOZ8POqvUg@megaproject-snapattend.tuf5i1s.mongodb.net/")
-db = client['SA-General']
-faculty_collection = db['Faculty']
 
-# Debug: Print all documents from faculty collection (optional)
-for doc in faculty_collection.find():
-    print(doc)
+# Get database
+db = client['SA-General']
+
+# Collections
+faculty_collection = db['Faculty']
+subject_collection=db['subject-class']
+
+
+
+
 
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
+    
     email = data.get('email')
     password = data.get('password')
 
@@ -31,6 +37,30 @@ def login():
         })
     else:
         return jsonify({'status': 'fail', 'message': 'Invalid email or password'}), 401
+
+
+@app.route('/get_subjects', methods=['POST'])
+def get_subjects():
+    data = request.get_json()
+    
+    classroom = data.get('class')
+    print(classroom)
+    
+    # Find subjects matching the class
+    subjects_cursor = subject_collection.find({'class': classroom})
+
+    # Extract only the 'course_name' fields
+    course_names = [subject.get('course_name') for subject in subjects_cursor]
+
+    if course_names:
+        return jsonify({
+            'status': 'success',
+            'subjects': course_names
+        })
+    else:
+        return jsonify({'status': 'fail', 'message': 'Invalid Classname......'}), 401
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
